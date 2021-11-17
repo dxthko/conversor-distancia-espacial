61d9d4c9629fd49bb4b6dac718d13df8bd0ab2f1
dxthko <h.sp97@hotmail.com>
2021-11-17 14:15:45 -0300

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 let nomePiloto = prompt('Para iniciar o uso do CDE, informe-nos o seu nome.')
 
-let greetings = prompt(
+let anosLuz = prompt(
   'Seja bem-vindo ' +
     nomePiloto +
     ', nos informe qual a sua distância em anos-luz.'
 )
 
-let opcoesVelocidade = alert(
+let opcoesVelocidade = prompt(
   nomePiloto +
-    ', você possui as seguintes opções: \n1. Parsec (pc);\n2. Unidade astronômica (AU);\n3. Quilômetros;'
+    ', você possui as seguintes opções: \n1. Parsec (pc);\n2. Unidade Astronômica (AU);\n3. Quilômetros;'
 )
 
-let velocidadeEscolhida = prompt(
-  nomePiloto + ', escolha uma das opções digitando o número respectivo.'
+let unidadeEscolhida
+let distanciaConvertida
+
+switch (opcoesVelocidade) {
+  case '1':
+    unidadeEscolhida = 'Parsec'
+    distanciaConvertida = anosLuz * 0.306601
+    break
+  case '2':
+    unidadeEscolhida = 'Unidade Astronômica'
+    distanciaConvertida = anosLuz * 63241.1
+    break
+  case '3':
+    unidadeEscolhida = 'Quilômetros'
+    distanciaConvertida = anosLuz * 9.5 * Math.pow(10, 12)
+    break
+  default:
+    unidadeEscolhida = 'Unidade não identificada'
+    distanciaConvertida = alert('Conversão fora do escopo.')
+    break
+}
+
+alert(
+  'Distância em anos-luz: ' +
+    anosLuz +
+    '\n' +
+    unidadeEscolhida +
+    ': ' +
+    distanciaConvertida
 )
